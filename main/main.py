cc9c18e19c68e77fe8570d6199fc5ebed108e357
lq <lq@lq-PC>
2017-01-15 21:26:05 +0800

    repair

--- main/main.py
+++ main/main.py
@@ -5,20 +5,34 @@ Created on 2017.1.8
 '''
 import threading
 import Queue
-from flask import Flask
+from time import sleep
+from flask import Flask 
 app = Flask(__name__)
 
 class master():
-    def __init__(self):
+    def __init__(self, num = 3):
         self.queue = Queue.Queue  
-        pass
+        self.threadList = []
+        self.threadNum = num
     
     def run(self):
-        app.add_url_rule('/hello', 'hello', self.hello, methods = ['GET', 'POST'])
+        #start thread
+        for i in range(self.threadNum):
+            self.threadList.append(threading.Thread(target=self.processThreadFunc, args = ()))
+        for p in self.threadList:
+            p.start()
+            
+        app.add_url_rule('/hello', 'hello', self.msgTransformationFunc, methods = ['GET', 'POST'])
         app.run(host='0.0.0.0', port='12345')
     
-    def processThreadFunc(self) :
+    def msgTransformationFunc(self):
         return 'hello word'
+       
+    def processThreadFunc(self):
+        while True:
+            sleep(3)
+            pass
+       
     
 if __name__ == '__main__':
     obj = master()

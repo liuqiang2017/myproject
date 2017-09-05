1feac421ad8841993f668bea477a51b4312f02c8
lq0216 <373346956@qq.com>
2017-09-05 16:44:31 +0800

    delete real info

--- main/config/default.py
+++ main/config/default.py
@@ -1,14 +1,4 @@
 class Config(object):
-    accounts_list = [{"admin":"intl.e2etest.1@service.aliyun.com", "password":"zrjyLWqO1"},\
-                   {"admin": "intl.e2etest.2@service.aliyun.com", "password": "CeqvzuMi1"},\
-                   {"admin": "intl.e2etest.3@service.aliyun.com", "password": "EsElpJQi1"},\
-                   {"admin": "intl.e2etest.4@service.aliyun.com", "password": "qYFSnXby1"},\
-                   {"admin": "intl.e2etest.5@service.aliyun.com", "password": "acnMGERT1"},\
-                   {"admin": "intl.e2etest.6@service.aliyun.com", "password": "oEaKJuTU1"},\
-                   {"admin": "intl.e2etest.7@service.aliyun.com", "password": "tDJtgvBE1"},\
-                   {"admin": "intl.e2etest.8@service.aliyun.com", "password": "QmnDHgMq1"},\
-                   {"admin": "intl.e2etest.9@service.aliyun.com", "password": "hWMAQnCL1"},\
-                   {"admin": "intl.e2etest.10@service.aliyun.com", "password": "ACouLyXN1"},\
-                   {"admin": "intl.e2etest@service.aliyun.com", "password": "FfmgKtlC1"}]
+    accounts_list = [{"admin":"intl.e2etest.1@service.aliyun.com", "password":"zrjyLWqO1"}]
     acc_timeout = 610
-    access_token = 'ce8e41cf37b58d2495a560f14fd3edbc7a33a93830e06cc74eb3bf9c14b672a5'
+    access_token = ''
